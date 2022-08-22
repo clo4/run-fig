c05c8426244a12c0fa27f1e57bfff9ce1bce3356
Robert <52195359+SeparateRecords@users.noreply.github.com>
2022-08-22 19:05:17 +1000

    Fix typos

--- src/parse.ts
+++ src/parse.ts
@@ -414,7 +414,7 @@ export function parse(
       if (!foundOptions.has(name)) {
         throw new ParseError(
           ctx(),
-          `${option.name} requires ${name}, add it to fix this erro`,
+          `${option.name} requires ${name}, add it to fix this error`,
         );
       }
     }
